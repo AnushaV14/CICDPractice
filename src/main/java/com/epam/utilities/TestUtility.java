e7220fef88a2851e56902c4d17155028ef0e413c
Anusha Vujjini <anusha_vujjini@epam.com>
2024-06-14 12:08:17 +0530

    added batch file

--- src/main/java/com/epam/utilities/TestUtility.java
+++ src/main/java/com/epam/utilities/TestUtility.java
@@ -2,13 +2,7 @@ package com.epam.utilities;
 
 public class TestUtility {
 
-	public static void main(String[] args) {
-		System.out.println("Test");
-		generateRandomNumber();
-	}
-
-	public static double generateRandomNumber() {
-
+	public double generateRandomNumber() {
 		return Math.random();
 	}
 
